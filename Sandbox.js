13fdcda7eb1af51a55659f2db7889bf37eb3f9bc
Granger <julian.granger@icloud.com>
2024-04-18 14:15:59 -0400

    hello world, stable

--- Sandbox.js
+++ Sandbox.js
@@ -1,10 +1,16 @@
 /*Programmer: Julian Granger
 Team Member: Travis Ralph 
-bolt ID:
+bolt ID:183F
 Date: 4-16-24
 Program: Hello World - Snadbox */
 
 async function startProgram() {
-	
-	// Write code here
+await speak("Hello World", true);
+	setMainLed({ r: 0, g: 0, b: 255});
+	setSpeed(60);
+	await delay (2);
+	setSpeed(0);
+
 }
+`
+
