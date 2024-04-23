64b5be5c110b82eef2927b1ee98bb15804be5e5d
Granger <julian.granger@icloud.com>
2024-04-23 13:32:27 -0400

    MyMiniCourse.js

---
+++ MiniCourse.js
@@ -0,0 +1,14 @@
+/*Programmer: Julian Granger
+Team Member: Travis Ralph 
+bolt ID:183F
+Date: 4-16-24
+Program: Hello World - mini course 
+Program URL https://edu.sphero.com/program/16743338/edit */
+
+
+async function startProgram() {
+	
+}
+
+
+
