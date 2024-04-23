3a57a2ef4d6971fb711328b48f29e3c9ed1e4593
Granger <julian.granger@icloud.com>
2024-04-23 14:16:19 -0400

    commit number 6

--- MiniCourse.js
+++ MiniCourse.js
@@ -16,4 +16,6 @@ async function startProgram() {
 	await roll((getHeading() + 90), 50, 2.5);
 	await delay(1)
 	await roll((getHeading() + 90), 50, 1);
+	await delay(1)
+	await roll((getHeading() + 90), 50, 1.25);
 }
