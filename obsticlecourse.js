6ee7791df5869f69ca185fba65647a952c8c8a77
Granger <julian.granger@icloud.com>
2024-04-24 14:17:41 -0400

    checkpoint number 5 - julian

--- obsticlecourse.js
+++ obsticlecourse.js
@@ -19,5 +19,10 @@ async function startProgram() {
 	await roll((getHeading() + 90), 50, 1);
 	await delay(1)
 	await roll((getHeading() - 90), 50, 1.5);
-	await delay(1);
+	await delay(1)
+	await roll((getHeading() - 90), 50, 0.1);
+	await delay(1)
+
+
+ 
 }
