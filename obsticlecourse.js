d5d2d320d24beb281af390807866f6c491768950
Granger <julian.granger@icloud.com>
2024-04-24 13:59:08 -0400

    checkpoint number 2 - travis ralph

--- obsticlecourse.js
+++ obsticlecourse.js
@@ -12,6 +12,8 @@ Program URL https://edu.sphero.com/program/16743338/edit */
 
 
 async function startProgram() {
-	await roll(0, 60, 2,);
-	awaitdelay(1)
+	await roll(0, 80, 1.5,);
+	await delay(1)
+	await roll(+90, 80, .75,);
+	await delay(1)
 }
