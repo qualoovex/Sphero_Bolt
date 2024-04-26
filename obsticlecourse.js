85081e680106cc33e6bcf8e6e18bcef1a1b13b92
Granger <julian.granger@icloud.com>
2024-04-26 13:48:55 -0400

    Julian Granger Pc 21 Real Checkpoint three

--- obsticlecourse.js
+++ obsticlecourse.js
@@ -10,7 +10,7 @@ Program URL https://edu.sphero.com/program/16743338/edit */
 
 
 
-
+// julian Granger Pc 21 Checkpoint one
 async function startProgram() {
 	await roll(0, 80, 1.5,);
 	await delay(1)
@@ -18,11 +18,16 @@ async function startProgram() {
 	await delay(1)
 	await roll((getHeading() + 90), 50, 1);
 	await delay(1)
+
+
+// Travis Ralph Pc 20 checkpoint two
 	await roll((getHeading() - 90), 50, 1.5);
 	await delay(1)
-	await roll((getHeading() - 90), 50, 0.1);
+	await roll((getHeading() - 90), 50, 0.75);
 	await delay(1)
 
-
+// Julian Granger Pc 21 Checkpoint three 
+	await roll((getHeading() - 45), 60, 2);
+	await delay(1)
  
 }
