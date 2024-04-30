576a17690336225f927e782300fa96b4aa66592f
Granger <julian.granger@icloud.com>
2024-04-30 13:28:46 -0400

    Added beginning comments

---
+++ MazeCompitition.js
@@ -0,0 +1,11 @@
+/*Programmer: Julian Granger
+Team Member: Travis Ralph 
+bolt ID:183F
+Date: 4-30-24
+Program: Obsticle course 
+Program URL - https://edu.sphero.com/program/16788350/edit*/
+
+async function startProgram() {
+	
+	
+}
