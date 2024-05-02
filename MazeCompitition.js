8f0898d640eb4ac1112762641e65f984c96d585a
Granger <julian.granger@icloud.com>
2024-05-02 13:21:07 -0400

    Travis Ralph - Start Text and Audio Checkpoint Commit

--- MazeCompitition.js
+++ MazeCompitition.js
@@ -1,11 +1,43 @@
-/*Programmer: Julian Granger
-Team Member: Travis Ralph 
-bolt ID:183F
-Date: 4-30-24
-Program: Obsticle course 
-Program URL - https://edu.sphero.com/program/16788350/edit*/
 
+//Programmer: Travis Ralph
+//Team Member: Julian Granger
+//Bolt ID:183F
+//Date: 4.30.2024
+//Program: Maze Competition
+//Program url: 
+
+
+	// Programmer: Travis Ralph - Start Text and Audio Checkpoint Commit - Date: 5/1/24
 async function startProgram() {
+	await speak('Start', true);
+	await scrollMatrixText("start", ({r: 90, g:200, b: 0}, 30, true);
+
+	// Programmer:  Julian Granger - Blue LED Checkpoint Commit - Date: 5/2/24
+
+
+
+	// Programmer:  - Sound One Checkpoint Commit - Date:
+
 	
-	
-}
+
+	// Programmer:  - Red LED Checkpoint Commit - Date:
+
+
+
+	// Programmer:  - Sound Two Checkpoint Commit - Date:
+
+
+
+	// Programmer:  - Green LED Checkpoint Commit - Date:
+
+
+
+	// Programmer:  - Purple LED and Sound Three  Checkpoint Commit - Date:
+
+
+
+	// Programmer:  - Finish Text and Audio Commit - Date:
+
+
+
+});
