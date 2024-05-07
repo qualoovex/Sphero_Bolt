81c745e9c7d7e006dec3ab7e0d3f4f2a4ddf08f6
Granger <julian.granger@icloud.com>
2024-05-07 14:07:53 -0400

    Travis Ralph - Sound Two Checkpoint Commit - Date: 5/7/24

--- MazeCompitition.js
+++ MazeCompitition.js
@@ -1,3 +1,4 @@
+
 //Programmer: Travis Ralph
 //Team Member: Julian Granger
 //Bolt ID:183F
@@ -13,34 +14,38 @@
 
 
 
-		// Programmer:  Julian Granger - Blue LED Checkpoint Commit - Date: 5/2/24
+		// Programmer: Julian Granger - Blue LED Checkpoint Commit - Date: 5/2/24
 			await roll((getHeading() + 0), 80, 1.9);
 			setMainLed({ r: 0, g: 0, b: 255 });
 			await delay(1);
 
-			// Programmer: Travis Ralph - Sound One Checkpoint Commit - Date:
+			// Programmer: Travis Ralph - Sound One Checkpoint Commit - Date: 5/3/24
 			await roll((getHeading() + 90), 80, 1.3);
 			await Sound.EightBit.Drop.play(true);
 			await delay(1);
 
-			// Programmer:Julian Granger  - Red LED Checkpoint Commit - Date:
+			// Programmer: Julian Granger - Red LED Checkpoint Commit - Date: 5/6/24
 			await roll((getHeading() + 90), 70, 0.9);
 			await delay(1);
-			await roll((getHeading() + 45), 70, 1);
+			await roll((getHeading() + 40), 70, 1);
 			await delay(1);
-			await roll((getHeading() - 100), 80, 0.6);
+			await roll((getHeading() - 100), 80, 0.65);
 			await delay(1);
 			setMainLed({ r: 255, g: 0, b: 0 });
+			await delay(1)
+
+			// Programmer: Travis Ralph - Sound Two Checkpoint Commit - Date: 5/7/24
+			await roll((getHeading() - 80), 70, 1);
+			await delay(1);
+			await roll((getHeading() + 90), 70, .55);
+			await Sound.EightBit.Chomp.play(true);
+			await delay(1) 
+
+
+
+			// Programmer:Julian Granger  - Green LED Checkpoint Commit - Date:5/7/24
+
 			
-
-			// Programmer:  - Sound Two Checkpoint Commit - Date:
-
-
-
-			// Programmer:  - Green LED Checkpoint Commit - Date:
-
-
-
 			// Programmer:  - Purple LED and Sound Three  Checkpoint Commit - Date:
 
 
